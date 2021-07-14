58b9a447d1abf5f02fae2230a91eeee72f116aa6
Alex <alexche8888@gmail.com>
2021-07-14 18:50:25 +0300

    add movie model

--- Cinema-MVC/App_Start/BundleConfig.cs
+++ Cinema-MVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Cinema_MVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
