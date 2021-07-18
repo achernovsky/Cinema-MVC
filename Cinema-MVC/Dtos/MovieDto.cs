5ef641834872a6b1377b2395dda10af1e8d0d4be
Alex <alexche8888@gmail.com>
2021-07-18 20:45:20 +0300

    Add DataAnnotations

--- Cinema-MVC/Dtos/MovieDto.cs
+++ Cinema-MVC/Dtos/MovieDto.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Cinema_MVC.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cinema_MVC.Dtos
 {
     public class MovieDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
---
+++ Cinema-MVC/Migrations/202107181744256_DataAnnotationsToMovie.cs
@@ -0,0 +1,18 @@
+namespace Cinema_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotationsToMovie : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+        }
+    }
+}
