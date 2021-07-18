40b46d5e4c008da4282a546175783d6318c1f8f2
Alex <alexche8888@gmail.com>
2021-07-18 12:51:28 +0300

    Add details page

--- Cinema-MVC/Controllers/MoviesController.cs
+++ Cinema-MVC/Controllers/MoviesController.cs
@@ -12,9 +12,9 @@ namespace Cinema_MVC.Controllers
     {
         private List<Movie> movies = new List<Movie>()
         {
-            new Movie() { Id = 1, Name = "Black Widow", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac dapibus justo, quis convallis nibh.",
+            new Movie() { Id = 1, Name = "Black Widow", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac dapibus justo, quis convallis nibh. Proin quis luctus nulla. Vivamus bibendum pellentesque consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam feugiat dui libero. Nam dui sem, laoreet et tellus sit amet, pretium finibus tellus. Etiam efficitur nec arcu vel vestibulum. Vestibulum ex nibh, tempor ut tempus a, consequat eget sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac dapibus justo, quis convallis nibh. Proin quis luctus nulla. Vivamus bibendum pellentesque consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam feugiat dui libero. Nam dui sem, laoreet et tellus sit amet, pretium finibus tellus. Etiam efficitur nec arcu vel vestibulum. Vestibulum ex nibh, tempor ut tempus a, consequat eget sem. ",
             ImageUrl = "https://hotcinema.co.il/images/BLACK-WIDOW.JPG?w=314&h=451&mode="},
-            new Movie() { Id = 2, Name = "Space Jam", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac dapibus justo, quis convallis nibh.",
+            new Movie() { Id = 2, Name = "Space Jam", Description = "Cras at nisl sed augue faucibus blandit. Suspendisse euismod pretium vehicula. Pellentesque lectus enim, dignissim sit amet congue sit amet, eleifend ut ligula. Vestibulum in rhoncus massa. Curabitur vitae ligula quis augue congue condimentum. Duis ut rutrum metus, eget iaculis nunc. Donec semper lorem sed ornare consequat. In tincidunt in dui a bibendum. Nam ultrices sagittis odio, non condimentum ex pharetra non. ",
             ImageUrl = "https://hotcinema.co.il/images/spaceJam_New.jpg?w=314&h=451&mode="}
         };
         public ActionResult Index()
@@ -27,7 +27,7 @@ namespace Cinema_MVC.Controllers
         }
         public ActionResult Details(int id)
         {
-            if (id >= movies.Count)
+            if (id > movies.Count)
                 return HttpNotFound();
 
             var movie = movies[id - 1];
