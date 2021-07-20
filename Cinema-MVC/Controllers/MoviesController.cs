97e7f343434a4f31bfd8f555f711de5d4d4a886c
Alex <alexche8888@gmail.com>
2021-07-20 13:03:11 +0300

    Add delete movie feature

--- Cinema-MVC/Controllers/MoviesController.cs
+++ Cinema-MVC/Controllers/MoviesController.cs
@@ -12,13 +12,6 @@ namespace Cinema_MVC.Controllers
 {
     public class MoviesController : Controller
     {
-        /*        private List<Movie> movies = new List<Movie>()
-                {
-                    new Movie() { Id = 1, Name = "Black Widow", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac dapibus justo, quis convallis nibh. Proin quis luctus nulla. Vivamus bibendum pellentesque consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam feugiat dui libero. Nam dui sem, laoreet et tellus sit amet, pretium finibus tellus. Etiam efficitur nec arcu vel vestibulum. Vestibulum ex nibh, tempor ut tempus a, consequat eget sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac dapibus justo, quis convallis nibh. Proin quis luctus nulla. Vivamus bibendum pellentesque consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam feugiat dui libero. Nam dui sem, laoreet et tellus sit amet, pretium finibus tellus. Etiam efficitur nec arcu vel vestibulum. Vestibulum ex nibh, tempor ut tempus a, consequat eget sem. ",
-                    ImageUrl = "https://hotcinema.co.il/images/BLACK-WIDOW.JPG?w=314&h=451&mode="},
-                    new Movie() { Id = 2, Name = "Space Jam", Description = "Cras at nisl sed augue faucibus blandit. Suspendisse euismod pretium vehicula. Pellentesque lectus enim, dignissim sit amet congue sit amet, eleifend ut ligula. Vestibulum in rhoncus massa. Curabitur vitae ligula quis augue congue condimentum. Duis ut rutrum metus, eget iaculis nunc. Donec semper lorem sed ornare consequat. In tincidunt in dui a bibendum. Nam ultrices sagittis odio, non condimentum ex pharetra non. ",
-                    ImageUrl = "https://hotcinema.co.il/images/spaceJam_New.jpg?w=314&h=451&mode="}
-                };*/
         private ApplicationDbContext _context;
 
         public MoviesController()
@@ -36,6 +29,7 @@ namespace Cinema_MVC.Controllers
             };
             return View(viewModel);
         }
+        
         public ActionResult Details(int id)
         {
             var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
@@ -56,8 +50,13 @@ namespace Cinema_MVC.Controllers
             var movie = new MovieDto();
             return View("MovieForm",movie);
         }
+
+        [ValidateAntiForgeryToken]
         public ActionResult Save(MovieDto movieDto)
         {
+            if (!ModelState.IsValid)
+                return View("MovieForm", movieDto);
+
             if(movieDto.Id == 0)
             {
                 var movie = Mapper.Map<MovieDto, Movie>(movieDto);
@@ -79,6 +78,18 @@ namespace Cinema_MVC.Controllers
 
             var movieDto = Mapper.Map<Movie, MovieDto>(movie);
             return View("MovieForm", movieDto);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction("", "Movies");
         }
     }
 }
